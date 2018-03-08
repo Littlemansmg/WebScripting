979b8bac04dbe6a8cabbe42823434b872fde51d1
LittlemanSMG <littlemansmg@gmail.com>
2018-03-08 14:05:29 -0600

    U1A2 Complete

---
+++ Lab02/unit1-a2.js
@@ -0,0 +1,12 @@
+document.write("<form>" +
+    "First Name: <input type='text'/><br/>" +
+    "Last Name: <input type='text'/><br/>" +
+    "Gender: <input type='radio' value='Male'/> <input type='radio' value='Female'/><br/>" +
+    "Phone: <select>" +
+    "<option value='home'>Home</option>" +
+    "<option value='cell'>Cell</option>" +
+    "<option value='work'>Work</option>" +
+    "</select> " +
+    "<input type='text'/><br/>" +
+    "<input type='submit' value='submit'>" +
+    "</form>")
