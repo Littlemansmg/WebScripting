4961bf26ed36a5ff947705f5c66a18cd18b62525
LittlemanSMG <littlemansmg@gmail.com>
2018-03-13 10:41:37 -0500

    U6A1 Done

---
+++ Unit6/U6A1/script.js
@@ -0,0 +1,14 @@
+if (location.search) {
+    var greeting = location.search;
+    greeting = greeting.replace("+", " ");
+    greeting = greeting.substring(greeting.lastIndexOf("=")
+    + 1);
+    document.getElementById("greetingtext").innerHTML =
+        decodeURIComponent(greeting);
+    }
+
+if (window.addEventListener) {
+    window.addEventListener("load", populateInfo, false);
+    } else if (window.attachEvent) {
+    window.attachEvent("onload", populateInfo);
+    }
